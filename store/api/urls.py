2daa90939183fcb235c291be3a26c10e6f5d0827
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-11-22 20:12:58 +0400

    add endpoints

--- store/api/urls.py
+++ store/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter as Router
 
-# from .views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
-#                     TagViewSet)
+from .views import (CustomUserViewSet, Product_groupViewSet, TypeViewSet,
+                    ProductViewSet, CartViewSet)
 
 router_v1 = Router()
-# router_v1.register('groups', TagViewSet, basename='group')
-# router_v1.register('types', RecipeViewSet, basename='type')
-# router_v1.register('products', IngredientViewSet, basename='product')
+router_v1.register('users', CustomUserViewSet, basename='user')
+router_v1.register('groups', Product_groupViewSet, basename='group')
+router_v1.register('types', TypeViewSet, basename='type')
+router_v1.register('products', ProductViewSet, basename='product')
+router_v1.register('cart', CartViewSet, basename='cart')
 
 
 urlpatterns = [
