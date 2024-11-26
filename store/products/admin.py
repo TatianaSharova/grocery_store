1e27a565c07f1042b1593e56f54055a77074a01f
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-11-26 23:41:13 +0400

    fixed admin zone for shopping carts

--- store/products/admin.py
+++ store/products/admin.py
@@ -7,6 +7,16 @@ class CartProductInline(admin.TabularInline):
     autocomplete_fields = ('product',)
     extra = MIN_NUM
     min_num = MIN_NUM
+    readonly_fields = ('product_price', 'total_price')
+    fields = ('product', 'amount', 'product_price', 'total_price')
+    
+    @admin.display(description='Цена за 1 шт')
+    def product_price(self, obj):
+        return obj.product.price
+    
+    @admin.display(description='Итоговая цена')
+    def total_price(self, obj):
+        return self.product_price(obj)*obj.amount
 
 
 class ProductImageInline(admin.TabularInline):
@@ -42,26 +52,38 @@ class TypeAdmin(admin.ModelAdmin):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'product_group', 'type',
-                    'slug', 'price',)
+                    'slug', 'price', 'in_stock')
     search_fields = ('name', 'slug')
     list_filter = ('type__product_group', 'type')
     inlines = (ProductImageInline,)
     empty_value_display = EMPTY_VALUE
-
+    
+    @admin.display(description='Категория')
     def product_group(self, obj):
         '''Отображает категорию в списке продуктов.'''
         return obj.product_group.name
-    product_group.short_description = 'Категория'
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     inlines = (CartProductInline,)
-    list_display = ('id', 'user', 'display_products')
+    list_display = ('id', 'user', 'display_products',
+                    'total_amount', 'total_price')
     search_fields = ('user__username',)
     empty_value_display = EMPTY_VALUE
 
     @admin.display(description='Продукты')
     def display_products(self, cart):
         return ', '.join([product.name for product in cart.products.all()])
+    
+    @admin.display(description='Итоговая стоимость')
+    def total_price(self, cart):
+        return sum(item.amount * item.product.price for item in cart.cartproduct_set.all())
+    
+    @admin.display(description='Количество товаров в корзине')
+    def total_amount(self, cart):
+        return sum(item.amount for item in cart.cartproduct_set.all())
+
+    
+
 
