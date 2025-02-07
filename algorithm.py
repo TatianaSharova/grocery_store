2005e279b28bdfb589a82069adc455158e6ff95e
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-02-07 15:39:33 +0400

    add Docker compose

--- algorithm.py
+++
@@ -1,9 +0,0 @@
-def numtonums(n: int) -> str:
-    s = ''
-    for i in range(1,n+1):
-        s = s + i*str(i)
-    return s
-
-if __name__ == '__main__':
-    number = int(input())
-    print(numtonums(number))
