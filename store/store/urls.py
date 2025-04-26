61a1e063f0b7d034fdc252ec478da597b01ffdeb
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-04-26 08:04:54 +0400

    delete debug  media urls

--- store/store/urls.py
+++ store/store/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
@@ -19,7 +17,3 @@ urlpatterns += [
     path('schema/redoc/',
          SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
